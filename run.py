a99bf4815c5e0c4a33c211f1c55c3fccae64c376
Prasetya <prasetya.career@gmail.com>
2026-06-05 15:04:15 +0700

    m

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
-print('hello world')
+from app import create_app
+
+if __name__ == "__main__":
+    app = create_app()
+
+    app.run(
+        debug=True,
+        use_reloader=True,
+        host='0.0.0.0',
+        port=5000
+        )
