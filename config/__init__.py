3dff0b23ac8f8963b760ba96d543a7dff882191c
Prasetya <prasetya.career@gmail.com>
2026-06-09 11:46:45 +0700

    m

--- config/__init__.py
+++ config/__init__.py
@@ -3,10 +3,22 @@ import traceback
 
 def register_config(app):
     try:
-        print("h")
+        from .logging.log_parser import (
+            app_logger,
+            access_logger,
+            security_logger,
+        )
+
+        app.app_logger = app_logger
+        app.access_logger = access_logger
+        app.security_logger = security_logger
+
+        app_logger.info("APP logger initialized")
+        access_logger.info("ACCESS logger initialized")
+        security_logger.info("SECURITY logger initialized")
 
     except Exception as e:
-        print("\nROUTE gagal di regis dan inisialisasi di create_app()")
+        print("\nLOGGER gagal diinisialisasi di create_app()")
         print("ERROR:", e)
 
         traceback.print_exc()
