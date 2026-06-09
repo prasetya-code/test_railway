6eeca773da4f8df1d5155c80c1195a954529ca07
Prasetya <prasetya.career@gmail.com>
2026-06-09 09:45:21 +0700

    testing

--- testing.bat
+++ testing.bat
@@ -4,17 +4,17 @@ setlocal
 echo ==================================
 echo Running DJLint Reformat...
 echo ==================================
-djlint app --reformat || goto :error
+djlint app --reformat
 
 echo.
 echo ==================================
 echo Running Stylint Fix...
 echo ==================================
 
-pushd node || goto :error
+pushd node
 echo Go to node directory
 
-call npm run fix_css || goto :error
+call npm run fix_css
 
 echo.
 echo Go back to root directory
@@ -24,25 +24,25 @@ echo.
 echo ==================================
 echo Running Ruff Auto Fix...
 echo ==================================
-ruff check --fix . || goto :error
+ruff check --fix .
 
 echo.
 echo ==================================
 echo Running Ruff Format...
 echo ==================================
-ruff format . || goto :error
+ruff format .
 
 echo.
 echo ==================================
 echo Running Ruff Recheck...
 echo ==================================
-ruff check . || goto :error
+ruff check .
 
 echo.
 echo ==================================
 echo Running Pytest...
 echo ==================================
-pytest || goto :error
+pytest
 
 echo.
 echo ==================================
